30975c9e14bc3c0fb1c472cda1595ff5c95c43da
Bulgantamir Gankhuyag <mr.unagaldai@gmail.com>
2015-04-12 18:57:30 +0900

    removed popup.js

--- popup.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  $.get('https://raw.githubusercontent.com/atulga/MNsticker/images/images.json', function (response) {
-    images = JSON.parse(response);
-    for (var i = 0; i < images.length; i ++) {
-      $('#images').append('<div class="col-md-4 col-xs-4 col-lg-4"><img src="' + images[i] + '" alt="" class="img-responsive"></div>');
-    }
-  });
-});
