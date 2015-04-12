fbccca4f9e8f00879437b89dd94b16b1635c76a0
Bulgantamir Gankhuyag <mr.unagaldai@gmail.com>
2015-04-12 18:56:48 +0900

    better design

--- extension/popup.js
+++ extension/popup.js
@@ -2,7 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
   $.get('https://raw.githubusercontent.com/atulga/MNsticker/images/images.json', function (response) {
     images = JSON.parse(response);
     for (var i = 0; i < images.length; i ++) {
-      $('#images').append('<div class="col-md-4 col-xs-4 col-lg-4"><img src="' + images[i] + '" alt="" class="img-responsive"></div>');
+      if ( i % 3 == 0 ) {
+        // Open row div
+        $('#images').append('<div class="row text-center">')
+      }
+
+      $('#images').append('<div class="col-md-4 col-xs-4 col-lg-4"><img src="' + images[i] + '" alt="" style="width: 160px; height: 160px"></div>');
+
+      if ( i % 3 == 2 ) {
+        // Close row div
+        $('#images').append('</div>')
+      }
     }
   });
 });
